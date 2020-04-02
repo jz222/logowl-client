719898136e1942e52dc3dea58230fa6224b25126
Timo <timotheus.zimmermann@me.com>
2020-04-02 14:07:34 +0200

    update dist

---
+++ dist/config.js
@@ -0,0 +1,4 @@
+window.loggy = {
+    // Set the URL of the LOGGY backend without the slash at the end
+    backendURL: 'http://localhost:2800'
+};
---
+++ dist/precache-manifest.e155f5f8c06ff983b9608ffd5c2ded59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74851f102db68808498cfb6aa9b8c7b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c642f187aa6573ccaa8",
+    "url": "/static/css/2.eb6dd4f5.chunk.css"
+  },
+  {
+    "revision": "635f50db565fc77e6d84",
+    "url": "/static/css/main.f7031aca.chunk.css"
+  },
+  {
+    "revision": "3c642f187aa6573ccaa8",
+    "url": "/static/js/2.22abd60b.chunk.js"
+  },
+  {
+    "revision": "3fc55b82b9dc6557779de2c87a4cda29",
+    "url": "/static/js/2.22abd60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635f50db565fc77e6d84",
+    "url": "/static/js/main.31e8057a.chunk.js"
+  },
+  {
+    "revision": "b6fdb4869deadf10c164",
+    "url": "/static/js/runtime-main.43a768b4.js"
+  }
+]);
