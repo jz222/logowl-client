4f8dc1ac97ceddcebb8da1f593dd9115c8660088
Timo <timotheus.zimmermann@me.com>
2020-04-03 15:31:18 +0200

    add comments

--- src/utils/expirationHandler.js
+++ src/utils/expirationHandler.js
@@ -1,3 +1,9 @@
+/**
+ * Calculates the expiration time and pushes the user back
+ * to the sign-in page if their JWT has been expired.
+ * @param history {object} history object that contains the push function
+ * @param expirationTime {int} expiration time of the JWT
+ */
 const expirationHandler = (history, expirationTime) => {
     const expiresIn = expirationTime - Date.now();
     
