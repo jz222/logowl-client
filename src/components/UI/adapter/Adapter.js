36566b2c179d9a0fe5d4dd7dcd08f10e5e1955fd
Timo <timotheus.zimmermann@me.com>
2020-05-31 16:23:00 +0200

    add custom adapter

--- src/components/UI/adapter/Adapter.js
+++ src/components/UI/adapter/Adapter.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 import nodejs from 'assets/logos/nodejs.png';
 import browser from 'assets/icons/browser.svg';
+import logowl from 'assets/logos/log-owl-uni.svg';
 
 import styling from './Adapter.module.scss';
 
 const Adapter = ({ type, size }) => {
     
-    let logo = nodejs;
+    let logo = logowl;
     
     if (type === 'browser') {
         logo = browser;
+    }
+    
+    if (type === 'nodejs') {
+        logo = nodejs;
     }
     
     return (
