060ba7960a3459262aaeeac49a209414f20ed3c0
Timo <timotheus.zimmermann@me.com>
2020-06-23 16:58:38 +0200

    disable plans

--- src/config/availablePlans.js
+++ src/config/availablePlans.js
@@ -5,19 +5,19 @@ export default {
             name: 'Free Plan',
             description: 'Includes 5000 requests/month and maximum one additional team member.',
             price: 'FREE'
-        },
-        {
-            id: 'business',
-            name: 'Business Plan',
-            description: 'Includes 5000 requests/month and maximum one additional team member.',
-            price: '15$'
-        },
-        {
-            id: 'scaleup',
-            name: 'Scale Up Plan',
-            description: 'Includes 5000 requests/month and maximum one additional team member.',
-            price: '45$'
         }
+        // {
+        //     id: 'business',
+        //     name: 'Business Plan',
+        //     description: 'Includes 5000 requests/month and maximum one additional team member.',
+        //     price: '15$'
+        // },
+        // {
+        //     id: 'scaleup',
+        //     name: 'Scale Up Plan',
+        //     description: 'Includes 5000 requests/month and maximum one additional team member.',
+        //     price: '45$'
+        // }
     ],
     freePlanId: 'free',
     mediumPlanId: 'business',
